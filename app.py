f1aa4afba4fabd5e0c76b9b0599efc301f591528
Chengao Jiang <jiangchengao@gmail.com>
2024-02-03 13:39:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-#import sqlite3
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sqlite3
+#__import__('pysqlite3')
+#import sys
+#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import gradio as gr
 from langchain.vectorstores import Chroma
